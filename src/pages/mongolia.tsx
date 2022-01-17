d08f530ede8d2716e79e714c2c72d9e01bdd5151
yanggak12 <yanggak12@naver.com>
2022-01-17 19:43:32 +0900

    chore: page files rename

---
+++ src/pages/mongolia.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function Mongolia({}: Props): ReactElement {
+    return (
+        <div>
+            Mongo
+        </div>
+    )
+}
