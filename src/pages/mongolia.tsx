b56b8080e2f8e23dacd674d4a854002a7efd3f4e
uuuuujin <yearn8546@gmail.com>
2022-01-19 17:23:25 +0900

    chore: seperate branch

--- src/pages/mongolia.tsx
+++ src/pages/mongolia.tsx
@@ -1,10 +1,23 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import BaseLayout from '../components/layout/BaseLayout';
+import axios from 'axios';
 
-export default function Mongolia(): ReactElement {
+const Mongolia: React.VFC = () => {
+  const baseUrl = 'https://disease.sh/v3/covid-19/countries/MN';
+  // const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <BaseLayout>
-      <div>Mongo</div>
+      <div>Mongolia Covid-19</div>
     </BaseLayout>
   );
-}
+};
+
+export default Mongolia;
